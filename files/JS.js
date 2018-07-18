0db93fd8085fa29956b745bb8ee95b7801c951a0
jiahao <yashichi@hotmail.com>
2018-07-18 17:49:16 +0800

    完成計算

--- files/JS.js
+++ files/JS.js
@@ -1,28 +1,58 @@
 //vue
+
+var test = new Vue({
+    el: '#test',
+    data: {
+        data1: 1,
+        data2: 1,
+        data3: 1,
+
+    },
+    methods: {
+        cal1: function () {
+            this.data3 = this.data1 * this.data2
+        },
+        cal2: function () {
+            this.data3 = this.data1 * this.data2
+        },
+        cal3: function () {
+            this.data2 = this.data3 / this.data1
+        }
+
+    }
+
+})
 var buttoncevent = new Vue({
-    el:'#buttonEvent',
-    methods:{
-        test:function(){
+    el: '#buttonEvent',
+    methods: {
+        test: function () {
             alert('成功')
         }
-        
+
     }
-    
+
 })
-Vue.component('product-input', { //商品列表input
-    props: ['comp_id'],
+Vue.component('product-input', { //商品列表input,
+    data: function () {//每個產生的元件皆有counter 變數，不共用
+        return {
+            countC: this.count1,
+            countP: this.count2,
+            countAP: this.count3
+        }
+    },
+    props: ['comp_id','count1','count2','count3'],
     template: '<div :id="\'product-\'+comp_id.toString()">\
 <div>\
 <input class="pdtinput" type="search" placeholder="品項">\
-<input :id="\'iso\'+comp_id.toString()" class="pdtinput" type="search" @change="putToproductlist(\'productIso\',comp_id)"  placeholder="國際條碼">\
+<input :id="\'iso\'+comp_id.toString()" class="pdtinput" type="search" @change="putToproductlist(\'productIso\',comp_id )"  placeholder="國際條碼">\
 <input :id="\'name\'+comp_id.toString()" class="pdtinput" type="search" @change="putToproductlist(\'productName\',comp_id)"  placeholder="商品名稱">\
 <input :id="\'type\'+comp_id.toString()" class="pdtinput" type="search" @change="putToproductlist(\'productType\',comp_id)"  placeholder="款式">\
 </div>\
 \
 <div>\
-<input :id="\'count\'+comp_id.toString()" type="search" v-on:focus.once="addinput" @change="putToproductlist(\'productCount\',comp_id)" placeholder="數量">\
-<input :id="\'price\'+comp_id.toString()" type="search" @change="putToproductlist(\'productPrice\',comp_id)"  placeholder="價格">\
-<input :id="\'allprice\'+comp_id.toString()" type="search" @change="putToproductlist(\'productAllpirce\',comp_id)"  placeholder="總價">\
+<input :id="\'count\'+comp_id.toString()" type="search" v-on:focus.once="addinput" @change="putToproductlist(\'productCount\',comp_id)" placeholder="數量" v-model="countC">\
+<input :id="\'price\'+comp_id.toString()" type="search" @change="putToproductlist(\'productPrice\',comp_id)"  placeholder="價格" v-model="countP">\
+<input :id="\'allprice\'+comp_id.toString()" type="search" @change="putToproductlist(\'productAllpirce\',comp_id)"  placeholder="總價" v-model="countAP">\
 </div>\
 </div>',
     methods: {
@@ -45,11 +75,17 @@ Vue.component('product-input', { //商品列表input
             } else if (a == 'productType') {
                 productlist.products[b].productType = $("#type" + b.toString()).val()
             } else if (a == 'productCount') {
+                this.countAP = this.countC*this.countP //計算總價
                 productlist.products[b].productCount = $("#count" + b.toString()).val()
+                productlist.products[b].productAllpirce = this.countAP //傳送總價
             } else if (a == 'productPrice') {
+                this.countAP = this.countC*this.countP //計算總價
                 productlist.products[b].productPrice = $("#price" + b.toString()).val()
+                productlist.products[b].productAllpirce = this.countAP //傳送總價 
             } else if (a == 'productAllpirce') {
+                this.countP = this.countAP/this.countC //計算單價
                 productlist.products[b].productAllpirce = $("#allprice" + b.toString()).val()
+                productlist.products[b].productPrice = this.countP //傳送總價 
             }
 
         }
@@ -91,7 +127,7 @@ var productlist = new Vue({ //商品列表資料
 
 })
 
-var webform = new Vue({//訂單客人資料
+var webform = new Vue({ //訂單客人資料
     el: '#orderform',
     data: {
         web: 'yahoo',
